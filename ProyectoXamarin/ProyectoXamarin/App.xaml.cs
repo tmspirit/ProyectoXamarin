4a0d663ef96f141ca8ba62ff60d51533c68d0256
Anthony Del Castillo Ruiz <anthony.delcastillo@tajamar365.com>
2020-05-16 12:30:39 +0200

    Vista Login falta funcionalidad

--- ProyectoXamarin/ProyectoXamarin/App.xaml.cs
+++ ProyectoXamarin/ProyectoXamarin/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoXamarin.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace ProyectoXamarin
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Login();
         }
 
         protected override void OnStart()

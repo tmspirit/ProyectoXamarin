8e0606ceb231647c653cd584f312385b29615052
Ivan Encinas Rodriguez <Ivan.Encinas@tajamar365.com>
2020-05-21 15:19:05 +0200

    Añadido que devuelva los productos dado un obvservablecollection<int>, falta que ese observablecollection se recoja de session

--- ProyectoXamarin/ProyectoXamarin/ViewModels/CarritoViewModel.cs
+++ ProyectoXamarin/ProyectoXamarin/ViewModels/CarritoViewModel.cs
@@ -1,18 +1,22 @@
 ﻿using ProyectoXamarin.Models;
 using System;
+using ProyectoXamarin.Repositories;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Runtime.CompilerServices;
+using ProyectoXamarin.Helpers;
 
 namespace ProyectoXamarin.ViewModels
 {
     public class CarritoViewModel
     {
+        private RepositoryMotores repo;
         private Carrito _Carrito;
-        public Carrito Carrito { 
+        public Carrito Carrito {
             get { return this._Carrito; }
             set {
                 this._Carrito.Productos = new ObservableCollection<int>();
@@ -20,8 +24,31 @@ namespace ProyectoXamarin.ViewModels
             }
         }
 
+        private NotifyTaskCompletion<ObservableCollection<Productos>> _TareaProductos;
+
+        //private ObservableCollection<Productos> _Productos;
+
+        public NotifyTaskCompletion<ObservableCollection<Productos>> TareaProductos {
+            get { return this._TareaProductos; }
+            set { 
+                this._TareaProductos = value;
+            }
+        }
+
         public CarritoViewModel() {
+            this.repo = new RepositoryMotores();
             this._Carrito = GenerarCarrito();
+            this._TareaProductos = 
+                new NotifyTaskCompletion<ObservableCollection<Productos>>(CargarProductos(this._Carrito.Productos));
+        }
+
+        public async Task<ObservableCollection<Productos>> CargarProductos(ObservableCollection<int> carrito){
+            ObservableCollection<Productos> productos = new ObservableCollection<Productos>();
+            foreach (int id_motor in carrito) {
+                Productos producto = await this.repo.GetProducto(id_motor);
+                productos.Add(producto);
+            }
+            return productos;
         }
 
         private Carrito GenerarCarrito()
